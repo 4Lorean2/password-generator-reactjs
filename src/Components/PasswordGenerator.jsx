0525489f7b2f0ff310648448600d85b2ca0f3097
Mustafa Taşdöğen <1Mustafatasdogen@gmail.com>
2024-05-17 23:06:55 +0300

    updated generator jsx (checkboxes) and css

--- src/Components/PasswordGenerator.jsx
+++ src/Components/PasswordGenerator.jsx
@@ -14,6 +14,34 @@ const PasswordGenerator = () => {
           </div>
         </div>
       </div>
+
+      <div className="setting">
+        <h3>Customize Your Password</h3>
+        <div className="customize">
+          <div className="checkboxes">
+            <div className="left">
+              <div className="checkbox-field">
+                <input type="checkbox" name="lower" id="lower" />
+                <label htmlFor="lower"> Include LowerCase(a-z) </label>
+              </div>
+              <div className="checkbox-field">
+                <input type="checkbox" name="upper" id="upper" />
+                <label htmlFor="upper"> Include UpperCase(A-Z) </label>
+              </div>
+            </div>
+            <div className="right">
+              <div className="checkbox-field">
+                <input type="checkbox" name="numbers" id="numbers" />
+                <label htmlFor="numbers"> Include Numbers(1-9) </label>
+              </div>
+              <div className="checkbox-field">
+                <input type="checkbox" name="symbols" id="symbols" />
+                <label htmlFor="symbols"> Include Symbols(&-#) </label>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
